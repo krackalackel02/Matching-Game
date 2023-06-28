d8f33c3a006c27171751397d25358715142ea3c4
Paul Kallarackel <108292404+krackalackel02@users.noreply.github.com>
2023-06-28 21:17:22 +0100

    added pulse effect

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 let images = document.querySelectorAll(".item img");
+let matchRemaining = document.querySelector("#Match .number");
+let totalMatches = images.length / 2;
+let matchesRemaining = totalMatches;
+matchRemaining.textContent = matchesRemaining;
 
 images.forEach((image) => {
 	image.addEventListener("click", (e) => {
 		e.target.parentElement.classList.add("flipped");
+		e.target.parentElement.classList.add("pulse-animation");
 		setTimeout(() => {
 			if (e.target.getAttribute("data-state") === "back") {
                 e.target.style.border = "2px solid black"
@@ -14,6 +19,10 @@ images.forEach((image) => {
 				e.target.setAttribute("data-state", "back");
 			}
 			e.target.parentElement.classList.remove("flipped");
+			e.target.parentElement.classList.remove("pulse-animation");
 		}, 250);
 	});
 });
+
+// Create a new click event
+let clickEvent = new Event("click");
