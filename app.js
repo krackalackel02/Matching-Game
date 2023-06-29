9c66991d6a5f3c4dc8ff5331da6355e908152cc1
Paul Kallarackel <108292404+krackalackel02@users.noreply.github.com>
2023-06-29 11:56:08 +0100

    overall project

--- app.js
+++ app.js
@@ -9,120 +9,122 @@ totalMisses.textContent = missCount;
 
 let flippedCards = []; // Array to store flipped cards
 let matchedCount = 0; // Number of matched pairs
-let clickable = true; // Variable to track if images are clickable
 
 images.forEach((image) => {
-  image.addEventListener("click", (e) => {
-    if (clickable) {
-      const card = e.target.parentElement;
+	image.addEventListener("click", (e) => {
+		const card = e.target.parentElement;
 
-      if (!card.classList.contains("flipped")) {
-        // Allow flipping only if the card is not already flipped
-        card.classList.add("flipped");
-        card.classList.add("pulse-animation");
+		if (!card.classList.contains("flipped")) {
+			// Allow flipping only if the card is not already flipped
+			card.classList.add("flipped");
+			card.classList.add("pulse-animation");
 
-        setTimeout(() => {
-          if (e.target.getAttribute("data-state") === "back") {
-            e.target.style.border = "2px solid black";
-            e.target.src = e.target.alt;
-            e.target.setAttribute("data-state", "front");
-          } else {
-            e.target.src = "./Images/question.png";
-            e.target.style.border = "2px solid white";
-            e.target.setAttribute("data-state", "back");
-          }
+			setTimeout(() => {
+				if (e.target.getAttribute("data-state") === "back") {
+					e.target.style.border = "2px solid black";
+					e.target.src = e.target.alt;
+					e.target.setAttribute("data-state", "front");
+				} else {
+					e.target.src = "./Images/question.png";
+					e.target.style.border = "2px solid white";
+					e.target.setAttribute("data-state", "back");
+				}
 
-          card.classList.remove("flipped");
-          card.classList.remove("pulse-animation");
+				card.classList.remove("flipped");
+				card.classList.remove("pulse-animation");
 
-          // Check if two cards are flipped
-          flippedCards.push(card);
+				// Check if two cards are flipped
+				flippedCards.push(card);
 
-          if (flippedCards.length === 2) {
-            clickable = false; // Disable click events temporarily
+				if (flippedCards.length === 2) {
+					// Disable click events temporarily to prevent selecting more cards
+					images.forEach((image) => {
+						image.parentElement.style.pointerEvents = "none";
+					});
 
-            // Check if the flipped cards match
-            const [card1, card2] = flippedCards;
-            const img1 = card1.querySelector("img");
-            const img2 = card2.querySelector("img");
+					// Check if the flipped cards match
+					const [card1, card2] = flippedCards;
+					const img1 = card1.querySelector("img");
+					const img2 = card2.querySelector("img");
 
-            if (img1 !== img2 && img1.alt === img2.alt) {
-              // Match found
-              matchedCount++;
+					if (img1 !== img2 && img1.alt === img2.alt) {
+						// Match found
+						matchedCount++;
 
-              // Keep the matched cards face up and add green border and pulse animation
-              img1.style.border = "2px solid green";
-              img2.style.border = "2px solid green";
-              img1.classList.add("pulse-animation-green");
-              img2.classList.add("pulse-animation-green");
+						// Keep the matched cards face up and add green border and pulse animation
+						img1.style.border = "2px solid green";
+						img2.style.border = "2px solid green";
+						img1.classList.add("pulse-animation-green");
+						img2.classList.add("pulse-animation-green");
+						
 
-              // Clear flippedCards array
-              flippedCards = [];
+						// Clear flippedCards array
+						flippedCards = [];
 
-              // Update matches remaining count
-              matchesRemaining--;
+						// Update matches remaining count
+						matchesRemaining--;
 
-              // Check if all matches have been found
-              if (matchedCount === totalMatches) {
-                // Game over
-                alert(
-                  `Congratulations! You've found ${totalMatches} matches  with only ${missCount} misses!`
-                );
-              }
+						// Check if all matches have been found
+						if (matchedCount === totalMatches) {
+							// Game over
+							alert(
+								`Congratulations! You've found ${totalMatches} matches  with only ${missCount} misses!`
+							);
+						}
+					} else {
+						// No match found
+						missCount++;
+						setTimeout(() => {
+							// Add red pulse animation to both cards
+							card1.classList.add("pulse-animation-red");
+							card2.classList.add("pulse-animation-red");
 
-              clickable = true; // Enable click events
-            } else {
-              // No match found
-              missCount++;
-              setTimeout(() => {
-                // Add red pulse animation to both cards
-                card1.classList.add("pulse-animation-red");
-                card2.classList.add("pulse-animation-red");
+							setTimeout(() => {
+								// Flip back the unmatched cards
+								img1.src = "./Images/question.png";
+								img1.style.border = "2px solid white";
+								img1.setAttribute("data-state", "back");
 
-                setTimeout(() => {
-                  // Flip back the unmatched cards
-                  img1.src = "./Images/question.png";
-                  img1.style.border = "2px solid white";
-                  img1.setAttribute("data-state", "back");
+								img2.src = "./Images/question.png";
+								img2.style.border = "2px solid white";
+								img2.setAttribute("data-state", "back");
 
-                  img2.src = "./Images/question.png";
-                  img2.style.border = "2px solid white";
-                  img2.setAttribute("data-state", "back");
+								// Remove red pulse animation classes
+								card1.classList.remove("pulse-animation-red");
+								card2.classList.remove("pulse-animation-red");
 
-                  // Remove red pulse animation classes
-                  card1.classList.remove("pulse-animation-red");
-                  card2.classList.remove("pulse-animation-red");
+								// Clear flippedCards array
+								flippedCards = [];
+							}, 1000);
+						}, 500);
+					}
 
-                  // Clear flippedCards array
-                  flippedCards = [];
+					// Update matches remaining count
+					matchRemaining.textContent = matchesRemaining;
+					// Update miss count
+					totalMisses.textContent = missCount;
 
-                  clickable = true; // Enable click events
-                }, 1000);
-              }, 500);
-            }
-
-            // Update matches remaining count
-
-            matchRemaining.textContent = matchesRemaining;
-            // Update miss count
-            totalMisses.textContent = missCount;
-          }
-        }, 250);
-      }
-    }
-  });
+					// Enable click events after a short delay
+					setTimeout(() => {
+						images.forEach((image) => {
+							image.parentElement.style.pointerEvents = "auto";
+						});
+					}, 1500);
+				}
+			}, 250);
+		}
+	});
 });
 
 let playAgain = document.getElementById("play-again");
 playAgain.addEventListener("click", () => {
-  location.reload();
+	location.reload();
 });
 
 function shuffleCards() {
-  const game = document.querySelector(".game");
-  for (let i = game.children.length; i >= 0; i--) {
-    game.appendChild(game.children[(Math.random() * i) | 0]);
-  }
+	const game = document.querySelector(".game");
+	for (let i = game.children.length; i >= 0; i--) {
+		game.appendChild(game.children[(Math.random() * i) | 0]);
+	}
 }
-
 window.addEventListener("DOMContentLoaded", shuffleCards);
